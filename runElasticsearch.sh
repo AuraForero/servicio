f23614bc8fef41bb3fbdb556a9319be855bd3070
Arbey Aragon <arbey.aragon@gmail.com>
2018-09-18 10:02:03 -0500

    Instalacion de docker

--- runElasticsearch.sh
+++ runElasticsearch.sh
@@ -1,3 +1,11 @@
+#Segun https://linuxconfig.org/how-to-install-docker-on-ubuntu-18-04-bionic-beaver
+sudo apt install docker.io
+
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo docker --version
+
 #Descargar el contenedor (Docker) de ES
 sudo docker pull docker.elastic.co/elasticsearch/elasticsearch:6.4.0
 
