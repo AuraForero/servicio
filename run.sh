8170d1a5083761e800586e077f3395b3e2c4b58e
Arbey Aragon <arbey.aragon@gmail.com>
2018-09-18 09:58:59 -0500

    Avance de Prueba técnica, el clienete web se conecta con la terminal de python pero aún no se conecta con elasticsearch

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
+#Para instalar el entorno virtual
 sudo apt install virtualenv
+
+#Para crear el entorno virtual
 virtualenv -p python3 env
+
+#Para ingresar al entorno virtual debe hacerse en la carpeta raiz
 source env/bin/activate
+
+#Para instalar las librerias necesarias
 pip install -r requirements.txt
--- runElasticsearch.sh
+++ runElasticsearch.sh
@@ -1,3 +1,6 @@
+#Descargar el contenedor (Docker) de ES
 sudo docker pull docker.elastic.co/elasticsearch/elasticsearch:6.4.0
+
+#Para correr el ES
 sudo docker run -it -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.4.0 bash
 
